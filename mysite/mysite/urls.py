6a3aaad7a0687ff0f3a05869a9fb4ea32fd36693
sureshatos <ksureshatos@gmail.com>
2020-08-14 12:18:30 +0530

    'final'

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap')
 ]
