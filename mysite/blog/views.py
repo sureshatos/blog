d68f302931502e49842e3bc39cec09eb6b49c464
sureshatos <ksureshatos@gmail.com>
2020-08-14 12:38:54 +0530

    'all'

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,13 +8,13 @@ from .models import Post,User
 from .forms import *
 from django.contrib import messages
 from taggit.models import Tag
-
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 def post_list(request, tag_slug=None):
     object_list = Post.published.all()
     t=Post.tags.all()
     tag = None
-
 
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
@@ -23,15 +23,13 @@ def post_list(request, tag_slug=None):
         print('obj',object_list)
 
 
-    paginator = Paginator(object_list, 2)  # 3 posts in each page
+    paginator = Paginator(object_list, 2) 
     page = request.GET.get('page',1)
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
-        # If page is not an integer deliver the first page
         posts = paginator.page(1)
     except EmptyPage:
-        # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
@@ -45,28 +43,20 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    
-    # List of active comments for this post
+
     comments = post.comments.filter(active=True)
 
     new_comment = None
 
     if request.method == 'POST':
-        # A comment was posted
         comment_form = CommentForm(data=request.POST or None)
         if comment_form.is_valid():
-            # Create Comment object but don't save to database yet
             new_comment = comment_form.save(commit=False)
-            # Assign the current post to the comment
             new_comment.post = post
-            # Save the comment to the database
             new_comment.save()
-            
             return redirect(post)
     else:
         comment_form = CommentForm()
-    
-     # List of similar posts
     post_tags_ids = post.tags.values_list('id', flat=True)
     print('post_tags_ids', post_tags_ids)
     print('post id',post.id)
@@ -97,21 +87,18 @@ class PostListView(ListView):
 
 
 def post_share(request, post_id):
-    # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
     sent = False
 
     if request.method == 'POST':
-        # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
-            # Form fields passed validation
             cd = form.cleaned_data
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = f"{cd['name']} recommends you read {post.title}"
             message = f"Read {post.title} at {post_url}\n\n" \
                       f"{cd['name']}\'s comments: {cd['comments']}"
-            send_mail(subject, message, 'suresh.kadari2@gmail.com',
+            send_mail(subject, message, 'userr@gmail.com',
                       [cd['to']], fail_silently=True)
             sent = True
 
@@ -120,3 +107,22 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post': post,
                                                     'form': form,
                                                     'sent': sent})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    result=[]
+    if 'query' in request.GET:
+        form=SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            result = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+                        
+    return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': result})
+
+
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from blog.sitemaps import PostSitemap
+from django.contrib.sitemaps.views import sitemap
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 sitemaps = {
     'posts': PostSitemap,
@@ -11,4 +16,4 @@ urlpatterns = [
     path('blog/', include('blog.urls', namespace='blog')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
